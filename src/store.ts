5f05d6a8cf8b618cf627a25a4bc362e152124eb8
Koppa <t@ioover.net>
2019-03-08 18:05:15 +0800

    store log middleware

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,20 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore, Middleware } from 'redux';
 import { tree } from './reducers';
 
 
-// const logger: Middleware = store => next => action => {
-//   console.group(action.type);
-//   console.info(action);
-//   let result = next(action);
-//   console.groupEnd();
-//   return result
-// };
+const logger: Middleware = () => next => action => {
+  console.group(action.type);
+  console.info(action);
+  let result = next(action);
+  console.groupEnd();
+  return result
+};
 
+// @ts-ignore
+const composeEnhancers: typeof compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   tree,
-  // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  {},
+  composeEnhancers(applyMiddleware(logger)),
 );
